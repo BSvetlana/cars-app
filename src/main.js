204038e849a0a97e994ae1c00c8472ec397c5526
BSvetlana <svetlanabisacki@gmail.com>
2018-05-08 18:43:35 +0200

    bootstrap and layouts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import VueRouter from "vue-router";
+
+import AppCars from './components/AppCars.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {path: '/', redirect: 'cars'},
+  {path: '/cars', component: AppCars}
+]
+
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
